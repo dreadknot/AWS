bdc6138d9a88eba0a62dd62febe8eb61dc0ad175
Mike Regan <dreadknot@gmail.com>
2014-10-10 14:18:53 -0700

    Update s3.py

--- boto/s3.py
+++ boto/s3.py
@@ -1,11 +1,7 @@
 >>> import boto
 >>> c = boto.connect_s3()
->>> b = c.get_bucket('splunkwhisper-backup-mregan-data')
->>> rs = bucket.list("FamilyPhotos")
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-NameError: name 'bucket' is not defined
->>> rs = b.list("mregan-idx2/config_backup")
+>>> b = c.get_bucket('bucket')
+>>> rs = b.list("dir/dir")
 >>> print rs
 <boto.s3.bucketlistresultset.BucketListResultSet object at 0x1439c90>
 >>> for key in rs:
