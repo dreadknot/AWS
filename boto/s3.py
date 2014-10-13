fabeafb9977d7c28b5fc9f31b5f891728a704e0b
Mike Regan <dreadknot@gmail.com>
2014-10-13 12:21:43 -0700

    Update s3.py

--- boto/s3.py
+++ boto/s3.py
@@ -1,9 +1,9 @@
->>> import boto
->>> c = boto.connect_s3()
->>> b = c.get_bucket('bucket')
->>> rs = b.list("dir/dir")
->>> print rs
+import boto
+c = boto.connect_s3()
+b = c.get_bucket('bucket')
+rs = b.list("dir/dir")
+print rs
 <boto.s3.bucketlistresultset.BucketListResultSet object at 0x1439c90>
->>> for key in rs:
+for key in rs:
 ...    print key.name
 ...
