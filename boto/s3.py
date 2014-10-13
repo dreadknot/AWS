af20142e3094b1926b72fa90888533007aee0cca
Mike Regan <dreadknot@gmail.com>
2014-10-13 13:07:37 -0700

    add lookup of bucket name

--- boto/s3.py
+++ boto/s3.py
@@ -1,6 +1,8 @@
 import boto
 c = boto.connect_s3()
 b = c.get_bucket('splunkwhisper-backup-mregan-data')
+d = c.lookup('splunkwhisper-backup-mregan-data')
+print d
 rs = b.list('mregan-idx2/config_backup/')
 print rs
 for key in rs:
