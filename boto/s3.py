1283bae555f53d9430e3c56bf6a0310c1686a5e0
Mike Regan <dreadknot@gmail.com>
2014-10-13 12:51:51 -0700

    add print last modified and size

--- boto/s3.py
+++ boto/s3.py
@@ -1,9 +1,7 @@
 import boto
 c = boto.connect_s3()
-b = c.get_bucket('bucket')
-rs = b.list("dir/dir")
+b = c.get_bucket('splunkwhisper-backup-mregan-data')
+rs = b.list('mregan-idx2/config_backup/')
 print rs
-<boto.s3.bucketlistresultset.BucketListResultSet object at 0x1439c90>
 for key in rs:
-...    print key.name
-...
+  print "%s %s" % (key.last_modified, key.size)
